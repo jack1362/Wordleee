8f76b3d2f53cd6abd1e368a4f0e8442e34e80711
CodedJack <86855940+jack1362@users.noreply.github.com>
2025-05-18 20:32:07 -0400

    Create wordList.py

---
+++ wordList.py
@@ -0,0 +1,8 @@
+List = ['cheer', 'pause', 'legal', 'plate', 'error', 'smash',
+'trade', 'burst', 'flash', 'shame', 'float', 'sting', 'boost',
+'alien', 'blend', 'party', 'store', 'shine', 'match', 'track',
+'route', 'crash', 'clown', 'shaky', 'vegan', 'power', 'trust',
+'enjoy', 'brain', 'adopt', 'tower', 'shade', 'delay', 'twist', 'alert', 'choke',
+'split', 'rhyme', 'muddy', 'plant', 'blind', 'sheet', 'crush', 'relax', 'drain',
+'label', 'expel', 'thump', 'quake', 'agree', 'event', 'limit', 'argue', 'sharp',
+'guide', 'march', 'image', 'worry', 'curse', 'grain']
